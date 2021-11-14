baf03825d5921085f0f2d1d44066201364a21bba
KabutoDrip <cammiller42003@gmail.com>
2021-11-13 19:24:05 -0700

    final prep commit

---
+++ BeautifulTest/headertest.js
@@ -0,0 +1,13 @@
+var wrap = $("#wrap");
+
+wrap.on("scroll", function(e) {
+    
+  if (this.scrollTop > 147) {
+    wrap.addClass("fix-search");
+  } else {
+    wrap.removeClass("fix-search");
+  }
+  
+});
+
+Resources
